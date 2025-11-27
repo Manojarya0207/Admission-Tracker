4b792199982d60a5140aede3103a15a4a7b40b43
Manojarya0207 <manojarya0207@gmail.com>
2025-11-27 11:31:52 +0530

    all file code with all correct

---
+++ admission_tranker/lib/principal/principal_home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PrincipalHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Principal Home")),
+      body: Center(child: Text("Welcome Principal")),
+    );
+  }
+}
---
+++ admission_tranker/lib/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text(
+          "Admission Tracker",
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
